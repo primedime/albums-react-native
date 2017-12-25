b1e413be939ab9c3d6e59f6f9a88f48a28872409
Roy Jossfolk <rjossfolk@gmail.com>
2017-12-24 21:43:40 -0500

    Display album.title in a list

--- components/AlbumList.js
+++ components/AlbumList.js
@@ -3,14 +3,23 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] };
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
     }
+
+    renderAlbums() {
+        return this.state.albums.map((album, i) => <Text key={i}>{album.title}</Text>);
+    }
+
     render() {
+        console.log(this.state);
+
         return (
             <View>
-                <Text>Album Lists</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
